6ac8258b167f800c909d5c6a2bbf3b696810f615
YourOwnSun <superprogamer132@gmail.com>
2021-03-06 20:40:05 +0400

    Импртировал оставшееся Яшино
    Привязал камеру к платформе

--- Student Simulator project/Assets/Scripts/ObjectControll.cs
+++ Student Simulator project/Assets/Scripts/ObjectControll.cs
@@ -15,7 +15,7 @@ public class ObjectControll : MonoBehaviour
 
     private void OnMouseDrag()
     {
-        Vector3 ScreenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraZDistance);
+        Vector3 ScreenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraZDistance*3);
         Vector3 NewWorldPosition = mainCamera.ScreenToWorldPoint(ScreenPosition);
         transform.position = NewWorldPosition;
     }
@@ -23,13 +23,13 @@ public class ObjectControll : MonoBehaviour
     //когда подвели мышь к объекту, он увеличивается
     void OnMouseEnter()
     {
-        transform.localScale = new Vector3(2.2f, 0.07f, 1.2f);
+        transform.localScale = new Vector3(2.2f, 0.001f, 1.43f);
     }
 
 
     //когда убрали мышь от объекта, он возвращается в исходную форму
     void OnMouseExit()
     {
-        transform.localScale = new Vector3(2f, 0.05f, 1f);
+        transform.localScale = new Vector3(2f, 0.001f, 1.3f);
     }
 }
