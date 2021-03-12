d32b60ecffcbb530ad69e4d6436e5bb35e76b026
YourOwnSun <superprogamer132@gmail.com>
2021-03-13 01:42:34 +0400

    Доработал скрипт CameraControl
    Добавил возможность того, что мышь двигается только по оси Y

--- Student Simulator project/Assets/Scripts/CameraControl.cs
+++ Student Simulator project/Assets/Scripts/CameraControl.cs
@@ -9,15 +9,9 @@ public class CameraControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(1))
+        if (Input.GetMouseButton(1))//Умножаем изменение координат на Time.deltatime, чтобы движение было независимо от частоты кадров
         {
-            if (Input.GetAxis("Mouse X") > 0)
-            {
-                transform.position -= new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed,
-                                           0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed);
-            }
-
-            else if (Input.GetAxis("Mouse X") < 0)
+            if (Input.GetAxis("Mouse X") != 0 || Input.GetAxis("Mouse Y") != 0)
             {
                 transform.position -= new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed,
                                            0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed);
