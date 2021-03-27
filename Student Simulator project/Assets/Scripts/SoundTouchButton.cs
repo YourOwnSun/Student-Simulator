86e81fd6f1f4382b413300a6b4c5dd29efe734aa
Parsifal <Jackob0301@gmail.com>
2021-03-27 23:51:07 +0400

    Звуки
    Добавил звуки нажатия и наведения на кнопки, но пока не регулируются

---
+++ Student Simulator project/Assets/Scripts/SoundTouchButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundTouchButton : MonoBehaviour
+{
+    public AudioSource sound;
+    public AudioClip onHover;
+    public AudioClip onClick;
+    public void HoverSound()
+    {
+        sound.PlayOneShot(onHover);
+    }
+
+    public void ClickSound()
+    {
+        sound.PlayOneShot(onClick);
+    }
+
+}
