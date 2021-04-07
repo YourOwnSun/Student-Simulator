05def45febe868febf83491b32cb801df7aac409
Parsifal <Jackob0301@gmail.com>
2021-04-08 00:32:47 +0400

    Пауза
    Сделал паузу с переходом в меню и выходом из игры, но пока не доработал (при паузе игра не замораживается)

---
+++ Student Simulator project/Assets/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject PauseGameUI;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Paused();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        PauseGameUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Paused()
+    {
+        PauseGameUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+        Debug.Log("Loading menu......");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting the game......");
+        Application.Quit();
+    }
+}
