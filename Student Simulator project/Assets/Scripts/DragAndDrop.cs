fc8232490d691a4004a6893967d9dc5088e1304e
YourOwnSun <superprogamer132@gmail.com>
2021-04-10 18:35:33 +0400

    НАКОНЕЦ ТО что-то похожее на драгындроп

--- Student Simulator project/Assets/Scripts/DragAndDrop.cs
+++ Student Simulator project/Assets/Scripts/DragAndDrop.cs
@@ -1,27 +1,69 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+[RequireComponent(typeof(Image))]
+public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    Vector2 offset;
-    public Canvas parentCanvasOfImageToMove;
+    public bool dragOnSurfaces = true;
 
-    public void OnBeginDrag(PointerEventData eventData) 
+    
+    private RectTransform m_DraggingPlane;
+
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        offset = new Vector2(transform.position.x - eventData.position.x, transform.position.y - eventData.position.y);
+        var canvas = FindInParents<Canvas>(gameObject);
+        if (canvas == null)
+            return;
+
+        // We have clicked something that can be dragged.
+        
+        if (dragOnSurfaces)
+            m_DraggingPlane = transform as RectTransform;
+        else
+            m_DraggingPlane = canvas.transform as RectTransform;
+
+        SetDraggedPosition(eventData);
     }
 
     public void OnDrag(PointerEventData data)
     {
-        Vector2 pos;
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(parentCanvasOfImageToMove.transform as RectTransform, data.position, parentCanvasOfImageToMove.worldCamera, out pos);
-        transform.position = parentCanvasOfImageToMove.transform.TransformPoint(pos);
+        SetDraggedPosition(data);
     }
 
-    public void OnEndDrag(PointerEventData eventData) 
+    private void SetDraggedPosition(PointerEventData data)
     {
+        if (dragOnSurfaces && data.pointerEnter != null && data.pointerEnter.transform as RectTransform != null)
+            m_DraggingPlane = data.pointerEnter.transform as RectTransform;
 
+        var rt = GetComponent<RectTransform>();
+        Vector3 globalMousePos;
+        if (RectTransformUtility.ScreenPointToWorldPointInRectangle(m_DraggingPlane, data.position, data.pressEventCamera, out globalMousePos))
+        {
+            rt.position = globalMousePos;
+            rt.rotation = m_DraggingPlane.rotation;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    static public T FindInParents<T>(GameObject go) where T : Component
+    {
+        if (go == null) return null;
+        var comp = go.GetComponent<T>();
+
+        if (comp != null)
+            return comp;
+
+        Transform t = go.transform.parent;
+        while (t != null && comp == null)
+        {
+            comp = t.gameObject.GetComponent<T>();
+            t = t.parent;
+        }
+        return comp;
     }
 }
