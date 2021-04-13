837878343995e124ff2303188c23c485eaffff8c
YourOwnSun <superprogamer132@gmail.com>
2021-04-13 10:32:55 +0400

    Чуть-чуть добавил

---
+++ Student Simulator project/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class DragAndDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public bool dragOnSurfaces = true;
+
+    private RectTransform m_DraggingPlane;
+
+    private void Start()
+    {
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        var canvas = FindInParents<Canvas>(gameObject);
+        if (canvas == null)
+            return;
+
+        // We have clicked something that can be dragged.
+
+        if (dragOnSurfaces)
+            m_DraggingPlane = transform as RectTransform;
+        else
+            m_DraggingPlane = canvas.transform as RectTransform;
+
+        SetDraggedPosition(eventData);
+        transform.SetAsLastSibling();
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+        SetDraggedPosition(data);
+    }
+
+    private void SetDraggedPosition(PointerEventData data)
+    {
+        if (dragOnSurfaces && data.pointerEnter != null && data.pointerEnter.transform as RectTransform != null)
+            m_DraggingPlane = data.pointerEnter.transform as RectTransform;
+
+        var rt = GetComponent<RectTransform>();
+        Vector3 globalMousePos;
+        if (RectTransformUtility.ScreenPointToWorldPointInRectangle(m_DraggingPlane, data.position, data.pressEventCamera, out globalMousePos))
+        {
+            rt.position = globalMousePos;
+            rt.rotation = m_DraggingPlane.rotation;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetAsFirstSibling();
+    }
+
+    static public T FindInParents<T>(GameObject go) where T : Component
+    {
+        if (go == null) return null;
+        var comp = go.GetComponent<T>();
+
+        if (comp != null)
+            return comp;
+
+        Transform t = go.transform.parent;
+        while (t != null && comp == null)
+        {
+            comp = t.gameObject.GetComponent<T>();
+            t = t.parent;
+        }
+        return comp;
+    }
+}
